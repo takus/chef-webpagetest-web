c8ef4b113f9443df75a20972d6dfae5b3de0a980
Tom Harrison <tharrison@hugeinc.com>
2015-04-15 17:42:24 -0400

    Ensure PHP5 is enabled in Apache2
    Add 'php5' to the ['apache']['default_modules'] attribute to prevent the
    server from responding with plaintext php code.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
 default['webpagetest-web']['documentroot'] = "/var/www"
+
+apache_modules = node['apache']['default_modules']
+if !apache_modules.include?('php5')
+  default['apache']['default_modules'] << 'php5'
+end
